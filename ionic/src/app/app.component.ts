00d8f89295ca6b569774ecf7d128e0e7c83a5092
kr3110 <n.nkr3110@gmail.com>
2017-02-18 13:12:09 -0500

    ditching ionic, moving to react-native

--- ionic/src/app/app.component.ts
+++ ionic/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { StatusBar } from 'ionic-native';
 import { TabsPage } from '../pages/tabs/tabs';
 import { AuthService } from '../services/auth/auth.service';
 
-
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
